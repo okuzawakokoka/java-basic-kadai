4d5f453deaa3c7c5719ff3cc8157f9c8c96495ec
okuzawakokoka <okuzawa_kokoka@koushi-intec.co.jp>
2026-04-23 10:49:34 +0900

    Add files via upload

---
+++ Jyanken_Chapter28/java/JyankenExec_Chapter28.java
@@ -0,0 +1,18 @@
+package Jyanken_Chapter28.java;
+
+public class JyankenExec_Chapter28 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		JyankenExec_Chapter28 jyanken = new JyankenExec_Chapter28();
+		
+		String my = Jyanken_Chapter28.getMyChoice();
+		String cpu = Jyanken_Chapter28.getRandom();
+		Jyanken_Chapter28.playGame(my,cpu);
+		
+		
+		
+	}
+
+}
